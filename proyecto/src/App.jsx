253238b2820080006a01daa94e3f614b4e76246e
JoseNB1207 <jose.ninob@uniagustiniana.edu.co>
2026-03-13 18:51:27 +0000

    commit2

--- proyecto/src/App.jsx
+++ proyecto/src/App.jsx
@@ -1,14 +1,30 @@
-import Jose from "./jose"
-import Bejarano from "./Bejarano"
-function App() {
- 
-  return (
-  <>
-    <Jose/>
+import StringState from "./StringState";
+import BooleanState from "./BooleanState";
+import NumberState from "./NumberState";
+import ObjectState from "./ObjectState";
+import ArrayState from "./ArrayState";
 
-    <Bejarano/>
-  </>
-  )
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Ejemplos de Estados en React</h1>
+
+      <h2>String</h2>
+      <StringState />
+
+      <h2>Boolean</h2>
+      <BooleanState />
+
+      <h2>Number</h2>
+      <NumberState />
+
+      <h2>Objeto</h2>
+      <ObjectState />
+
+      <h2>Array</h2>
+      <ArrayState />
+    </div>
+  );
 }
 
-export default App
+export default App;
